a6692b95aa32816a30fe7ec4df50337d2bf7d107
dv-formateur <dvermonden@yahoo.fr>
2023-01-09 13:06:43 +0100

    feat: add id property to contacts

--- data.js
+++ data.js
@@ -1,9 +1,11 @@
 export const contacts = [
   {
+    id: "OrvKFV",
     nom: "Xavier",
     telephone: "0434343434",
   },
   {
+    id: "PJPsE5",
     nom: "Robert",
     telephone: "0534343434",
   },
